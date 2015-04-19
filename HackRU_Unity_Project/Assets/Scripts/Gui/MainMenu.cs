c7ffd1b2db35ea72648fb36f689d950f8cdcc7cd
Flafla2 <awesomesauceface@aol.com>
2015-04-19 04:12:16 -0400

    Head tracking, Wiimote Connect system, Main Menu - Added head tracking algorithm influenced by Johnny Lee - Added a handoff Wiimote connect system - Added a Main Menu GUI

---
+++ HackRU_Unity_Project/Assets/Scripts/Gui/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using WiimoteApi;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField]
+    private Animator m_MenuAnimator;
+    [SerializeField]
+    private float m_UpdateRate = 1;
+    [SerializeField]
+    private NunchuckInputModule m_InputModule;
+
+    private float LastWiimoteUpdate;
+    private bool HasWiimotes;
+    private bool HasSetupGun;
+
+    void Start()
+    {
+        WiimoteManager.FindWiimote(true);
+        LastWiimoteUpdate = Time.time;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Time.time - LastWiimoteUpdate > m_UpdateRate)
+        {
+            WiimoteManager.FindWiimote(false);
+            LastWiimoteUpdate = Time.time;
+        }
+
+        HasWiimotes = WiimoteManager.Wiimotes.Count >= 2;
+        HasSetupGun = HasWiimotes && !WiimoteOptions.WaitingForGunUpdate;
+
+        if (!HasWiimotes)
+            WiimoteOptions.RedefineRemoteIDs();
+
+        m_MenuAnimator.SetBool("HasWiimotes", HasWiimotes);
+        m_MenuAnimator.SetBool("HasSetupGun", HasSetupGun);
+
+        if (HasSetupGun)
+            m_InputModule.wiimote = WiimoteOptions.GunRemote;
+	}
+}
---
+++ HackRU_Unity_Project/Assets/Scripts/HeadTracking/WiimoteOptions.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using WiimoteApi;
+
+public class WiimoteOptions : MonoBehaviour {
+
+    public static Wiimote HeadTrackingRemote
+    {
+        get { return _HeadTrackingRemote;  }
+    }
+    private static Wiimote _HeadTrackingRemote;
+
+    public static Wiimote GunRemote
+    {
+        get { return _GunRemote; }
+    }
+    private static Wiimote _GunRemote;
+
+    public static bool WaitingForGunUpdate {
+        get { return _WaitingForGunUpdate; }
+    }
+    public static bool _WaitingForGunUpdate = true;
+
+    public static float WiimoteReadInterval = 1f / 30f;
+    private static float LastWiimoteReadTime = 0;
+
+    void Start()
+    {
+        //WiimoteManager.Debug_Messages = true;
+    }
+
+    void Update()
+    {
+        if (Time.time - LastWiimoteReadTime > WiimoteReadInterval)
+        {
+            foreach (Wiimote remote in WiimoteManager.Wiimotes)
+            {
+                int ret;
+                do
+                {
+                    ret = WiimoteManager.ReadWiimoteData(remote);
+                } while (ret > 0);
+            }
+            LastWiimoteReadTime = Time.time;
+        }
+        
+
+        if (WaitingForGunUpdate && WiimoteManager.Wiimotes.Count >= 2)
+        {
+            for (int x = 0; x < WiimoteManager.Wiimotes.Count; x++ )
+            {
+                if (WiimoteManager.Wiimotes[x].b)
+                {
+                    int other = 1;
+                    if(x == 1) other = 0;
+                    _GunRemote = WiimoteManager.Wiimotes[x];
+                    _HeadTrackingRemote = WiimoteManager.Wiimotes[other];
+
+                    x = WiimoteManager.Wiimotes.Count;
+                    _WaitingForGunUpdate = false;
+
+                    WiimoteManager.SendPlayerLED(_GunRemote, true, true, true, false);
+                    WiimoteManager.SendPlayerLED(_HeadTrackingRemote, true, false, false, true);
+
+                    WiimoteManager.SetupIRCamera(_GunRemote, WiimoteManager.IRDataType.BASIC);
+                    WiimoteManager.SetupIRCamera(_HeadTrackingRemote, WiimoteManager.IRDataType.EXTENDED);
+                }
+            }
+        }
+    }
+
+    public static void RedefineRemoteIDs()
+    {
+        _WaitingForGunUpdate = true;
+    }
+}
